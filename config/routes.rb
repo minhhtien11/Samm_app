08eec1af83a76fa1080ce267c62e33f30aa8faa0
blacktho <vanquytho12345@gmail.com>
2022-03-28 10:07:09 +0700

    done log_out

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
   get 'login'  => 'sessions#new'
-post  'login' => 'sessions#create'
-delete 'logout' => 'sessions#destroy'
+  post  'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
 end
