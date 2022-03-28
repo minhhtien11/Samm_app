4ba471917c0142f2f5373e31deba95f7e79dae3e
blacktho <vanquytho12345@gmail.com>
2022-03-28 15:38:02 +0700

    update_delete

--- Gemfile
+++ Gemfile
@@ -26,7 +26,7 @@ gem "stimulus-rails"
 
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
-
+gem "faker"
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
@@ -44,8 +44,13 @@ gem "bootsnap", require: false
 
 # Use Sass to process CSS
 gem "sassc-rails"
+
 gem 'bootstrap-sass', '3.4.1'
 
+gem "will_paginate"
+
+
+gem "bootstrap-will_paginate"
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
+  before_action :correct_user, only: [:edit, :update]
   def show 
     @user = User.find(params[:id])
   end
-
+  def index
+    @users = User.paginate(page: params[:page])
+  end
   def new
     @user = User.new
   end
@@ -17,8 +21,42 @@ class UsersController < ApplicationController
     render 'new'
     end
   end
+
+  def destroy
+    user = User.find_by(id: params[:id])
+    if user&.destroy
+    flash[:success] = "User deleted"
+    else
+    flash[:danger] = "Delete fail!"
+    end
+    redirect_to users_url
+  end
+
+  def edit 
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+      if @user.update(user_params)
+        flash[:success] = " Profile update"
+        redirect_to @user
+    # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
   private
   def user_params
     params.require(:user).permit(:name, :email, :password,:password_confirmation)
+  end
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
   end
 end
